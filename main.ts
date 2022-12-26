bfa03e4585bd7f586f4b291b7e5081943728821d
joe-rugezo <118993541+joe-rugezo@users.noreply.github.com>
2022-12-26 16:48:54 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,10 @@
+let hungriness = 0
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        hungriness = hungriness + 1
+        basic.showNumber(hungriness)
+    } else if (input.buttonIsPressed(Button.B)) {
+        hungriness = 0
+        basic.showNumber(hungriness)
+    }
+})
